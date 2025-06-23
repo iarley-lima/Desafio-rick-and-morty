49f0f153d4eb8039a43627acb074968c408cfe75
Iarley Lima <202213600018@ifba.deu.br>
2025-06-23 11:58:56 -0300

    segundo commit

--- src/pages/Cards/index.jsx
+++ src/pages/Cards/index.jsx
@@ -1,41 +1,53 @@
-import { Box, HStack, Image, Tag, Heading, Center, Spinner, SimpleGrid, Text, Button, Flex } from "@chakra-ui/react"
+import { Box, Image, Heading, Spinner, SimpleGrid, Text, Button, Flex, Input, Center } from "@chakra-ui/react";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Link } from 'react-router-dom';
-import { Input } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 function Cards() {
   const [personagens, setPersonagens] = useState([]);
   const [loading, setLoading] = useState(true);
   const [busca, setBusca] = useState("");
+//----------------- axios e API --------------------
   useEffect(() => {
     axios
-      .get('https://rickandmortyapi.com/api/character/[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]')
+      .get(
+        "https://rickandmortyapi.com/api/character/[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]"
+      )
       .then((res) => {
-        setPersonagens(res.data); 
+        setPersonagens(res.data);
         setLoading(false);
       })
       .catch((err) => {
-        console.error('Erro ao buscar personagens:', err);
+        console.error("Erro ao buscar personagens:", err);
         setLoading(false);
       });
-}, []);
+  }, []);
 
-
+  //------------------------- responsividade ------------------------------
+  if (loading) {
+    return (
+      <Center h="100vh" bg="gray.900">
+        <Spinner size="xl" color="green.400" />
+      </Center>
+    );
+  }
+  
+//------------------------ barra de pesquisa --------------------------
   return (
-    <Box bg="gray.900" color="white" py="10" minH="100vh">
-    <Flex justify="center" mb={6}>
-      <Input
-        placeholder="Pesquisar personagem..."
-        value={busca}
-        onChange={(e) => setBusca(e.target.value)}
-        mb={6}
-        bg="white"
-        color="black"
-        maxW="400px"
-      />
-    </Flex>
-      <SimpleGrid columns={[1, 2, 3, 4]} spacing="8" px="10">
+    <Box bg="gray.900" color="white" py={10} minH="100vh" px={{ base: 4, md: 10 }}>
+      <Flex justify="center" mb={6}>
+        <Input
+          placeholder="Pesquisar personagem..."
+          value={busca}
+          onChange={(e) => setBusca(e.target.value)}
+          bg="white"
+          color="black"
+          maxW="400px"
+          w="100%"
+        />
+      </Flex>
+
+      <SimpleGrid columns={{ base: 1, sm: 2, md: 3, lg: 4 }} spacing={8}>
         {personagens
           .filter((p) =>
             p.name.toLowerCase().includes(busca.toLowerCase())
@@ -44,30 +56,33 @@ function Cards() {
             <Box
               key={personagem.id}
               bg="white"
-              p="6"
+              color="black"
+              p={5}
               borderRadius="md"
-              boxShadow="md"
+              boxShadow="lg"
               maxW="300px"
               mx="auto"
-              >
-            <Image
+              textAlign="center"
+            >
+              <Image
                 src={personagem.image}
                 alt={`Foto de ${personagem.name}`}
                 borderRadius="xl"
                 objectFit="cover"
                 mx="auto"
-            />
-            <Heading my="4" size="md" textAlign="center" color="Black">
+                mb={4}
+              />
+              <Heading fontSize="lg" mb={2}>
                 {personagem.name}
-            </Heading>
-          
-          <Link to={`/personagem/${personagem.id}`}>
-            <Button size="sm" colorScheme="green" mt={2}>Ler mais</Button>
-          </Link>
+              </Heading>
 
-
-          </Box>
-        ))}
+              <Link to={`/personagem/${personagem.id}`}>
+                <Button size="sm" colorScheme="green" mt={2}>
+                  Ler mais
+                </Button>
+              </Link>
+            </Box>
+          ))}
       </SimpleGrid>
     </Box>
   );
